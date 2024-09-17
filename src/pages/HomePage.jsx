32f9a2b3d237de8cdfc0ff6fca60ee674aef745f
吕佩宗 <lvpeizong@lvpeizongdeMac-mini.local>
2024-09-18 01:52:37 +1000

    add dragging

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import ImageContainer from '../components/ImageContainer';
+import axios from 'axios';
+import './HomePage.css';
+
+function HomePage() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/get-image-url').then((data, err) => {
+      if (err) {
+        console.log(err);
+      }
+      setImages(data.data);
+      console.log(data.data);
+    });
+  }, []);
+  return <ImageContainer images={images} />;
+}
+
+export default HomePage;
